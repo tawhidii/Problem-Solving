c5bbbf83ac550e6fd24663eba6b80530d4605b48
Khondoker Khademul Bari <barii.py@gmail.com>
2022-05-16 16:03:36 +0600

    Updated/bin/python3

--- Random/bubble-sort.py
+++ Random/bubble-sort.py
@@ -8,7 +8,7 @@ class Solution:
     def bubbleSort(self,arr, n):
         # code here
         for i in range(n-1):
-            for j in range(n-1):
+            for j in range(n-1-i):
                 if arr[j]>arr[j+1]:
                     arr[j],arr[j+1] = arr[j+1],arr[j]
         return arr
