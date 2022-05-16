b2465e40aa5a23a95a87e5ab4ac513257ce3f392
Khondoker Khademul Bari <barii.py@gmail.com>
2022-05-16 16:02:57 +0600

    Updated

---
+++ Random/bubble-sort.py
@@ -0,0 +1,19 @@
+#User function Template for python3
+
+from re import A
+
+
+class Solution:
+    #Function to sort the array using bubble sort algorithm.
+    def bubbleSort(self,arr, n):
+        # code here
+        for i in range(n-1):
+            for j in range(n-1):
+                if arr[j]>arr[j+1]:
+                    arr[j],arr[j+1] = arr[j+1],arr[j]
+        return arr
+
+
+s = Solution()
+print(s.bubbleSort([4, 1, 3, 9, 7],5))
+        
