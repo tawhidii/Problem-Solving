c8a3a951d48b48f6af35b38001d37760e409506c
Bari <barii.py@gmail.com>
2022-10-01 11:48:28 +0600

    Added

---
+++ Hackerrank/Sorting/missing-numbers.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'missingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY arr
+ *  2. INTEGER_ARRAY brr
+ */
+
+function missingNumbers(arr, brr) {
+    // Write your code here
+    arr.forEach((val)=>{
+        let index = brr.indexOf(val)
+        brr.splice(index,1)
+    })
+    let ans  = brr.sort((a,b)=>a-b)
+    return [...new Set(ans)]
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const m = parseInt(readLine().trim(), 10);
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const result = missingNumbers(arr, brr);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
